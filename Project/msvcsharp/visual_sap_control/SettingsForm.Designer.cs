2a762a22002359b6d7819f28ba3122c61bce822c
Yuriy Lipchey <xaosmmm@gmail.com>
2021-01-23 15:37:52 +0200

    added autoresize to datagreed

--- Project/msvcsharp/visual_sap_control/SettingsForm.Designer.cs
+++ Project/msvcsharp/visual_sap_control/SettingsForm.Designer.cs
@@ -8,6 +8,7 @@ namespace msvcsharp.visual_sap_control
 		/// </summary>
 		private System.ComponentModel.IContainer components = null;
 		private System.Windows.Forms.DataGridView dataGridView;
+		private System.Windows.Forms.Label fontLabel;
 		
 		/// <summary>
 		/// Disposes resources used by the form.
@@ -31,6 +32,7 @@ namespace msvcsharp.visual_sap_control
 		private void InitializeComponent()
 		{
 			this.dataGridView = new System.Windows.Forms.DataGridView();
+			this.fontLabel = new System.Windows.Forms.Label();
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
 			this.SuspendLayout();
 			// 
@@ -39,14 +41,24 @@ namespace msvcsharp.visual_sap_control
 			this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
 			this.dataGridView.Location = new System.Drawing.Point(12, 12);
 			this.dataGridView.Name = "dataGridView";
-			this.dataGridView.Size = new System.Drawing.Size(879, 328);
+			this.dataGridView.Size = new System.Drawing.Size(879, 122);
 			this.dataGridView.TabIndex = 0;
+			// 
+			// fontLabel
+			// 
+			this.fontLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.fontLabel.Location = new System.Drawing.Point(12, 254);
+			this.fontLabel.Name = "fontLabel";
+			this.fontLabel.Size = new System.Drawing.Size(100, 23);
+			this.fontLabel.TabIndex = 1;
+			this.fontLabel.Text = "fontLabel";
 			// 
 			// SettingsForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(914, 497);
+			this.Controls.Add(this.fontLabel);
 			this.Controls.Add(this.dataGridView);
 			this.MaximizeBox = false;
 			this.MinimizeBox = false;
--- Project/msvcsharp/visual_sap_control/SettingsForm.cs
+++ Project/msvcsharp/visual_sap_control/SettingsForm.cs
@@ -24,41 +24,59 @@ namespace msvcsharp.visual_sap_control
 		}
 		void SettingsFormLoad(object sender, EventArgs e)
 		{
+			dataGridView.Font = fontLabel.Font;
+			
 			var column1 = new DataGridViewColumn();
-            column1.HeaderText = "ProductName"; //текст в шапке
-            column1.Width = 100; //ширина колонки
-            column1.ReadOnly = true; //значение в этой колонке нельзя править
-            column1.Name = "ProductName"; //текстовое имя колонки, его можно использовать вместо обращений по индексу
-            column1.Frozen = true; //флаг, что данная колонка всегда отображается на своем месте
-            column1.CellTemplate = new DataGridViewTextBoxCell(); //тип нашей колонки
+			column1.HeaderText = "Product"; //текст в шапке
+			column1.Name = "ProductName"; //текстовое имя колонки, его можно использовать вместо обращений по индексу
+			column1.CellTemplate = new DataGridViewTextBoxCell(); //тип нашей колонки
 
-            var column2 = new DataGridViewColumn();
-            column2.HeaderText = "BarcodeExample"; 
-            column2.Name = "BarcodeExample";
-            column2.CellTemplate = new DataGridViewTextBoxCell();
+			var column2 = new DataGridViewColumn();
+			column2.HeaderText = "BarcodeExample"; 
+			column2.Name = "BarcodeExample";
+			column2.CellTemplate = new DataGridViewTextBoxCell();
 
-            var column3 = new DataGridViewColumn();
-            column3.HeaderText = "BarcodeMask";
-            column3.Name = "BarcodeMask";
-            column3.CellTemplate = new DataGridViewTextBoxCell();
+			var column3 = new DataGridViewColumn();
+			column3.HeaderText = "BarcodeMask";
+			column3.Name = "BarcodeMask";
+			column3.CellTemplate = new DataGridViewTextBoxCell();
             
-            var column4 = new DataGridViewColumn();
-            column4.HeaderText = "FirmwareFile";
-            column4.Name = "FirmwareFile";
-            column4.CellTemplate = new DataGridViewTextBoxCell();
+			var column4 = new DataGridViewColumn();
+			column4.HeaderText = "FirmwareFile";
+			column4.Name = "FirmwareFile";
+			column4.CellTemplate = new DataGridViewTextBoxCell();
             
-            var column5 = new DataGridViewColumn();
-            column5.HeaderText = "NfcFile";
-            column5.Name = "NfcFile";
-            column5.CellTemplate = new DataGridViewTextBoxCell();
+			var column5 = new DataGridViewColumn();
+			column5.HeaderText = "NfcFile";
+			column5.Name = "NfcFile";
+			column5.CellTemplate = new DataGridViewTextBoxCell();
 
-            dataGridView.Columns.Add(column1);
-            dataGridView.Columns.Add(column2);
-            dataGridView.Columns.Add(column3);
-            dataGridView.Columns.Add(column4);
-            dataGridView.Columns.Add(column5);
+			dataGridView.Columns.Add(column1);
+			dataGridView.Columns.Add(column2);
+			dataGridView.Columns.Add(column3);
+			dataGridView.Columns.Add(column4);
+			dataGridView.Columns.Add(column5);
+			
+			dataGridView.Rows.Add();
+			dataGridView.ReadOnly = true;
 
-            dataGridView.AllowUserToAddRows = false; //запрешаем пользователю самому добавлять строки
+			dataGridView.AllowUserToAddRows = false; //запрешаем пользователю самому добавлять строки
+			for (int i = 0; i < dataGridView.Columns.Count - 1; i++) {
+				dataGridView.Columns[i].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+			}
+			dataGridView.Columns[dataGridView.Columns.Count - 1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+
+			for (int i = 0; i < dataGridView.Columns.Count; i++) {
+				int colw = dataGridView.Columns[i].Width;
+				dataGridView.Columns[i].AutoSizeMode = DataGridViewAutoSizeColumnMode.None;
+				dataGridView.Columns[i].Width = colw;
+			}
+			
+			
+		    //	dataGridView.AutoResizeColumns();            
+			//dataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+			//dataGridView.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+			//dataGridView.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
 	
 		}
 		void SettingsFormFormClosed(object sender, FormClosedEventArgs e)
