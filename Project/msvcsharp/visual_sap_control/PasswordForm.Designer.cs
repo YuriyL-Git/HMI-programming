f80c7e718726bbc07ee0b09ed0eb8588495b53fb
Yuriy Lipchey <xaosmmm@gmail.com>
2021-01-23 22:28:12 +0200

    added password form

--- Project/msvcsharp/visual_sap_control/PasswordForm.Designer.cs
+++ Project/msvcsharp/visual_sap_control/PasswordForm.Designer.cs
@@ -7,6 +7,8 @@ namespace msvcsharp.visual_sap_control
 		/// Designer variable used to keep track of non-visual components.
 		/// </summary>
 		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button buttonOk;
+		private System.Windows.Forms.TextBox passwordTextBox;
 		
 		/// <summary>
 		/// Disposes resources used by the form.
@@ -29,12 +31,40 @@ namespace msvcsharp.visual_sap_control
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.buttonOk = new System.Windows.Forms.Button();
+			this.passwordTextBox = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// buttonOk
+			// 
+			this.buttonOk.Location = new System.Drawing.Point(115, 118);
+			this.buttonOk.Name = "buttonOk";
+			this.buttonOk.Size = new System.Drawing.Size(75, 32);
+			this.buttonOk.TabIndex = 0;
+			this.buttonOk.Text = "button1";
+			this.buttonOk.UseVisualStyleBackColor = true;
+			this.buttonOk.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// passwordTextBox
+			// 
+			this.passwordTextBox.Location = new System.Drawing.Point(90, 29);
+			this.passwordTextBox.Name = "passwordTextBox";
+			this.passwordTextBox.Size = new System.Drawing.Size(156, 22);
+			this.passwordTextBox.TabIndex = 1;
+			this.passwordTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.PasswordTextBoxKeyDown);
 			// 
 			// PasswordForm
 			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "PasswordForm";
+			this.ClientSize = new System.Drawing.Size(353, 253);
+			this.Controls.Add(this.passwordTextBox);
+			this.Controls.Add(this.buttonOk);
 			this.Name = "PasswordForm";
+			this.Text = "PasswordForm";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 	}
 }
--- Project/msvcsharp/visual_sap_control/PasswordForm.cs
+++ Project/msvcsharp/visual_sap_control/PasswordForm.cs
@@ -21,5 +21,25 @@ namespace msvcsharp.visual_sap_control
 			// TODO: Add constructor code after the InitializeComponent() call.
 			//
 		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			if (passwordTextBox.Text == "Jabil12345") {
+				
+				this.DialogResult = DialogResult.OK;
+			} else {
+				// The password is invalid.
+				passwordTextBox.Clear();
+				MessageBox.Show("Inivalid password.");
+				passwordTextBox.Focus();
+			}
+	
+		}
+		private void PasswordTextBoxKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+				buttonOk.PerformClick();
+            }
+        }
 	}
 }
